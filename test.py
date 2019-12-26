76519b5881afd1696d9265e1db14fa1467268956
Samuel Potter <sfp@Samuels-MacBook-Air.local>
2019-12-26 13:50:54 -0500

    update test file

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
+import olim
 import meshio
 import numpy as np
-import python_example as olim
 
 mesh = meshio.read('sphere3.vtk')
 
 points = mesh.points.astype(np.float64)
+print('|points| = %d' % (points.shape[0],))
+
 tetra = mesh.cells['tetra'].astype(np.int64)
+print('|tetra| = %d' % (tetra.shape[0],))
 
 S = lambda x: 1 + np.dot(x, [0.2, -0.3, 0.1])
-
 slowness = S(points)
 
-solver = olim.EikonalTetraSolver(points, tetra, slowness)
+tol = np.finfo(np.float64).resolution
+solver = olim.EikonalAdaptiveGaussSeidel(points, tetra, slowness, tol)
+
+i0 = np.argmin(np.sum(points**2, axis=1))
+print('|points[i0]| = %g' % np.linalg.norm(points[i0]))
+
+solver.add_boundary_point(i0, 0.0)
+solver.commit()
+solver.solve()
